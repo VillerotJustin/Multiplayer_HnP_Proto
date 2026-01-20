18b69a7da51f27162f6e43facf29bf6a7fcf59b8
VillerotJustin <62113284+VillerotJustin@users.noreply.github.com>
2026-01-20 19:01:26 +0100

    WaitForPlayerState

---
+++ Assets/GameStates/WaitForPlayerState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using PurrNet.StateMachine;
+using UnityEngine;
+
+public class WaitForPlayerState : StateNode
+{
+    [SerializeField] private int minPlayers = 2;
+    
+    public override void Enter(bool asServer)
+    {
+        base.Enter(asServer);
+
+        StartCoroutine(WaitForPlayers());
+    }
+
+    private IEnumerator WaitForPlayers()
+    {
+        while (networkManager.players.Count < minPlayers)
+            yield return null;
+        
+        machine.Next();
+    }
+}
