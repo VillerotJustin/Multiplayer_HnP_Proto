f516ff502e8387862c48a6d46c32fe8b095548aa
VillerotJustin <62113284+VillerotJustin@users.noreply.github.com>
2026-01-21 15:00:08 +0100

    Moving files

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -8,7 +8,11 @@ public class Gun : NetworkBehaviour
     [SerializeField] private LayerMask hitLayer;
     [SerializeField] private float range = 20f;
     [SerializeField] private int damage = 10;
-
+    [SerializeField] private float fireRate = 0.5f;
+    [SerializeField] private bool autoFire = true;
+    [SerializeField] private ParticleSystem muzzleFlash;
+    
+    private float _lastFireTime;
 
     protected override void OnSpawned()
     {
@@ -19,8 +23,14 @@ public class Gun : NetworkBehaviour
 
     private void Update()
     {
-        if (!Input.GetKeyDown(KeyCode.Mouse0))
+        if ((autoFire && !Input.GetKey(KeyCode.Mouse0)) || (!autoFire && !Input.GetKeyDown(KeyCode.Mouse0)) )
             return;
+        
+        if (_lastFireTime + fireRate > Time.unscaledTime)
+            return;
+
+        PlayShotEffect();
+        _lastFireTime = Time.unscaledTime;
         
         if (!Physics.Raycast(cameraTransform.position, cameraTransform.forward, out RaycastHit hitInfo, range, hitLayer))
             return;
@@ -31,8 +41,12 @@ public class Gun : NetworkBehaviour
             return;
         
         playerHealth.ChangeHealth(-damage);
-        
-
-
+    }
+    
+    [ObserversRpc(runLocally: true)]
+    private void PlayShotEffect()
+    {
+        if (muzzleFlash)
+            muzzleFlash.Play();
     }
 }
